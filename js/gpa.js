ae53d433b0e71749618785275a65f3f1f21f3019
XinYang-YXY <yu.xin.yang.yxy@gmail.com>
2019-07-19 12:31:44 +0800

    FInished calculate and add module buttons, left with add semester button

--- js/gpa.js
+++ js/gpa.js
@@ -6,11 +6,81 @@ if(document.readyState == "loading"){
 
 
 function ready(){
-
   var moduleButton = document.getElementsByClassName("moduleButton")[0];
   moduleButton.addEventListener("click", addAnotherModule);
+
+  var calculateButton = document.getElementsByClassName("calculateButton")[0];
+  calculateButton.addEventListener("click", calculateResult);
+
+  var addFormButton = document.getElementsByClassName("addFormButton")[0];
+  addFormButton.addEventListener("click", addForm);
 }
 
+function addForm(event){
+  var button = event.target;
+  var forms = button.parentElement.parentElement;
+  alert(forms);
+} //End Function [Add Form]
+function calculateResult(event){
+  var totalGrade = 0;
+  var totalCredit = 0;
+  var finalResult = 0;
+
+  var button = event.target;
+  var forms = button.parentElement.parentElement.getElementsByClassName("grade");
+  //Loop through the forms
+  for(var i =0; i< forms.length; i++){
+    var form = forms[0];
+    var modules = form.getElementsByClassName("modules")[0].getElementsByClassName("oneModule");
+    //Loop through the modules
+    for(var i=0; i<modules.length; i++){
+      var oneModule = modules[i];
+
+      var moduleGrade = oneModule.getElementsByClassName("moduleGrade")[0].value;
+      var moduleCredit = oneModule.getElementsByClassName("moduleCredit")[0].value;
+
+      if(moduleCredit == "" || moduleGrade == "NA"){
+
+      } else{
+        switch (moduleGrade) {
+          case "A":
+            totalGrade += (4 * moduleCredit);
+            break;
+          case "B+":
+            totalGrade += (3.5 * moduleCredit);
+            break;
+          case "B":
+            totalGrade += (3 * moduleCredit);
+            break;
+          case "C+":
+            totalGrade += (2.5 * moduleCredit);
+            break;
+          case "C":
+            totalGrade += (2 * moduleCredit);
+            break;
+          case "D+":
+            totalGrade += (1.5 * moduleCredit);
+            break;
+          case "D":
+            totalGrade += (1 * moduleCredit);
+            break;
+          case "F":
+            totalGrade += 0;
+            break;
+          default:
+            alert("Grade Translation went wrong, please report to site adminstrator!!!");  //Hidden
+        } //End Switch
+        totalCredit += parseFloat(moduleCredit);
+        //alert(totalGrade); //Testing is the switch working
+      } //End ELse
+
+    } //End for modules loop
+  } //End for forms loop
+
+  finalResult = totalGrade / totalCredit;
+  alert(finalResult);
+  //Calculate FUnction //
+} //End function [Calculate]
 function addAnotherModule(event){
   var button = event.target;
   var moduleField =  button.parentElement.parentElement.getElementsByClassName("modules")[0];
@@ -47,19 +117,8 @@ function addAnotherModule(event){
   <label>Credits:</label>
     <input type="number" class="moduleCredit" value="">
   </div>
-  `;
+  `;  //HIdden
   oneModule.innerHTML = oneModuleContent;
 
   moduleField.appendChild(oneModule);
-
-}
-
-function display(){
-  var moduleName = document.getElementsByClassName("moduleName")[0].value;
-  var moduleGrade = document.getElementsByClassName("moduleGrade")[0].value;
-  var moduleCredit = document.getElementsByClassName("moduleCredit")[0].value;
-
-  alert(moduleName);
-  alert(moduleGrade);
-  alert(moduleCredit);
-}
+} //End FUnction [Add Module]

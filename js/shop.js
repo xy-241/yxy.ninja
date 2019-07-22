e319788f0b37a99eff3d89dd6eb0b61d560811d5
XinYang-YXY <yu.xin.yang.yxy@gmail.com>
2019-07-22 20:14:40 +0800

    Doing the cart layout

--- js/shop.js
+++ js/shop.js
@@ -12,13 +12,57 @@ function ready(){
   }
   //Tab Actions
 
+  //Go to cart button
   var goToShoppingCart = document.getElementsByClassName("goToShoppingCart")[0];
   goToShoppingCart.addEventListener("click", showShoppingCart);
+  //Go to cart button
+
+  //Go Back Button
+  var goBack = document.getElementsByClassName("goBack")[0];
+  goBack.addEventListener("click", showShoppingItems);
+  //Go Back Button
+
+
+  //Checkout Button
+  var checkout = document.getElementsByClassName("checkout")[0];
+  checkout.addEventListener("click", justFourSweetie);
+
+  //Checkout Button
 }
 
+function justFourSweetie(event){
+  alert("Sweetie, you are cute 💖 ;)");
+}
+
+//Go To Cart
 function showShoppingCart(event){
+  //HIde the shopping items
+  document.getElementsByClassName("sellingItems")[0].style.display = "none";
+  document.getElementsByClassName("itemsAvailable")[0].style.display = "none";
+  document.getElementsByClassName("goToShoppingCart")[0].style.display = "none";
+  //HIde the shopping items
 
+  //Display shopping cart
+  document.getElementsByClassName("shoppingCart")[0].style.display = "block";
+  document.getElementsByClassName("goBack")[0].style.display = "block";
+  document.getElementsByClassName("checkout")[0].style.display = "block";
+  //Display shopping cart
 }
+//Go Back
+function showShoppingItems(event){
+  //Display the shopping items
+  document.getElementsByClassName("sellingItems")[0].style.display = "block";
+  document.getElementsByClassName("itemsAvailable")[0].style.display = "block";
+  document.getElementsByClassName("goToShoppingCart")[0].style.display = "block";
+  //Display the shopping items
+
+  //Hide shopping cart
+  document.getElementsByClassName("shoppingCart")[0].style.display = "none";
+  document.getElementsByClassName("goBack")[0].style.display = "none";
+  document.getElementsByClassName("checkout")[0].style.display = "none";
+  //Hide shopping cart
+}
+
 function displayItems(event){
   //Get the item Page Name
   var button = event.target;
